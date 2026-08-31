a4e95c78d150ee27e06a902f5d4e173f70bdde17
Satyam Gupta <2006satyamgupta@gmail.com>
2026-08-31 23:49:15 +0530

    [Syncode] GeeksForGeeks/Easy/Make the array beautiful

---
+++ GeeksForGeeks/Easy/Make the array beautiful/solution.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        // code here
+        stack<int> s;
+        
+        for(int i =0; i<arr.size();i++){
+            // stack empty
+            if(s.empty()){
+                s.push(arr[i]);
+                
+            }
+            // non negative number
+            else if(arr[i]>=0){
+                
+                //non negative number
+                if(s.top()>=0){
+                    s.push(arr[i]);
+                }
+                // negative
+                else{
+                    s.pop();
+                }                
+                
+            }
+            
+            
+            else{
+            //negative number
+                if(s.top()<0){
+                    s.push(arr[i]);
+                }
+            //non negative numebr
+                else{
+                s.pop();
+                    }
+                }
+            }
+            
+            vector<int> ans(s.size());
+            int i = s.size()-1;
+            
+            while(!s.empty())
+            {
+                ans[i] = s.top();
+                i--;
+                s.pop();
+            }
+            return ans;
+        }
+    
+};
