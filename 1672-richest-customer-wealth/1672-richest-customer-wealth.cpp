23c79957e8ce137cc75b94ef6bd6d253edf55dba
Satyam Gupta <2006satyamgupta@gmail.com>
2026-08-31 16:27:16 +0530

    Time: 0 ms (100%), Space: 11.7 MB (8.85%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int wealthSum = 0;
-        for(auto &money: accounts){
+        for(auto money: accounts){
             int curr = 0;
             for(int x: money){
                 curr+=x;
